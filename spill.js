2bbeeccbdc1cfefa195965b2998a1cfb45287081
Magnus Helvik Forsdahl <mh.forsdahl@stud.uis.no>
2025-02-15 21:12:26 +0100

    litt ymse

--- spill.js
+++ spill.js
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if (this.checked) {
             bilde.classList.add("visible");
             bilde.classList.remove("hidden");
-            antall.value = 5;
+            antall.value = 1;
         } else {
             bilde.classList.add("hidden");
             bilde.classList.remove("visible");
@@ -62,4 +62,50 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+    //sjekker kullkraftverk
+
+    document.getElementById("kullkraft").addEventListener("change", function() {
+        let bilde = document.getElementById("kullkraftverkb");
+        let antall = document.getElementById("kullkraft_mengde");
+        if (this.checked) {
+            bilde.classList.add("visible");
+            bilde.classList.remove("hidden");
+            antall.value = 2;
+        } else {
+            bilde.classList.add("hidden");
+            bilde.classList.remove("visible");
+            antall.value = 0;
+        }
+    });
+
+    //endre verdi på slider vind
+
+    document.getElementById("vindstyrke").addEventListener("input", function() {
+        let slider = document.getElementById("vindstyrke");
+        let output = document.getElementById("vind_variasjons_tall");
+        output.innerHTML = slider.value;
+    });
+
+    // endre verdi på variasjonstall
+
+    document.getElementById("variasjon").addEventListener("input", function() {
+        let slider = document.getElementById("variasjon");
+        let output = document.getElementById("variasjons_tall");
+        output.innerHTML = (slider.value * 100).toFixed(0);
+    });
+
+    // endre verdi på slider temperatur
+
+    document.getElementById("temperatur").addEventListener("input", function() {
+        let slider = document.getElementById("temperatur");
+        let output = document.getElementById("temperatur_variasjons_tall");
+        output.innerHTML = slider.value;
+    });
+
+    //endre verdi på skydekke
+    document.getElementById("skydekke").addEventListener("input", function() {
+        let slider = document.getElementById("skydekke");
+        let output = document.getElementById("skydekke_variasjons_tall");
+        output.innerHTML = (slider.value*100).toFixed(0);
+    });
 });
